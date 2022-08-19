9786c17c73104a7796eab2b0b94063f8630d2539
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-08-19 15:44:41 +0900

    Add files via upload

---
+++ JAVA/pro01/src/quiz02/Quiz02.java
@@ -0,0 +1,12 @@
+package quiz02;
+
+public class Quiz02 {
+
+	public static void main(String[] args) {
+		int result = 3 + 8 * (3-1);
+		System.out.println(result);
+		double result1 = 2*3+5/2.0;
+		System.out.println(result1);
+	}
+
+}
---
+++ JAVA/pro01/src/quiz03/Quiz03.java
@@ -0,0 +1,16 @@
+package quiz03;
+
+public class Quiz03 {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 3;
+		System.out.println(6/3);
+		System.out.println(6%3);
+		System.out.println(30 - 2 * (5+4));
+		System.out.println(++x * y--);
+		System.out.println(x * y);
+
+	}
+
+}

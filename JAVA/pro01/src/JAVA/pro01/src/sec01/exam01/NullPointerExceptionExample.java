54410a28d7a71f1e3ecb8c6c52f6a925689a1f2c
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-08-19 15:50:13 +0900

    Delete JAVA/pro01/src/JAVA/pro01/src/sec01 directory

--- JAVA/pro01/src/JAVA/pro01/src/sec01/exam01/NullPointerExceptionExample.java
+++
@@ -1,8 +0,0 @@
-package sec01.exam01;
-
-public class NullPointerExceptionExample {
-	public static void main(String[] args) {
-		String data = null;
-		System.out.println(data.toString());
-	}
-}
--- JAVA/pro01/src/JAVA/pro01/src/sec01/exam02/RemoteControl.java
+++
@@ -1,6 +0,0 @@
-package sec01.exam02;
-
-public interface RemoteControl {
-	int MAX_VOLUME = 10;
-	int MIN_VOLUME = 10;
-}

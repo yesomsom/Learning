20986e63e6a27ab5f5f8d1a323a3339918800dd2
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-08-22 10:47:42 +0900

    Add files via upload

---
+++ JSP&Spring/pro26/src/main/java/com/spring/ex02/LoginVO.java
@@ -0,0 +1,27 @@
+package com.spring.ex02;
+
+public class LoginVO {
+	private String userID;
+	private String userName;
+	private String email;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}

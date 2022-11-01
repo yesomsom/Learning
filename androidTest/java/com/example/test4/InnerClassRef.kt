5be74def3ae30520f42f3c97f674db2d9abf4441
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-11-01 11:02:17 +0900

    Add files via upload

---
+++ androidTest/java/com/example/test4/InnerClassRef.kt
@@ -0,0 +1,30 @@
+package com.example.test4
+
+open class Base {
+    open val x: Int = 1
+    open fun f() = println("Base Class f()")
+}
+class Child : Base() {
+    override val x: Int = super.x + 1
+    override fun f() = println("Child Class f()")
+
+    inner class  Inside {
+        fun f() = println("Inside Class f()")
+        fun test() {
+            f()
+            Child().f()
+            super@Child.f()
+            println("[Inside] super@Child.x: ${super@Child.x}")
+        }
+    }
+}
+fun main() {
+    val c1 = Child()
+    c1.Inside().test()
+}
+/*
+Inside Class f()
+Child Class f()
+Base Class f()
+[Inside] super@Child.x: 1
+ */

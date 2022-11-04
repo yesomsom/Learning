bd34b3cb8cadf72abeb389e150bc46b608e1c42f
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-11-05 01:34:45 +0900

    Add files via upload

---
+++ study/board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.study.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+    @GetMapping("/board/write") //localhost:8090/board/write
+    public String boardWriteForm() {
+        return "boardWrite";
+    }
+}

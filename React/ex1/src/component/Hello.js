c2623e49bde45312734d248e989b8973cb2a4cb5
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-12-04 00:02:18 +0900

    Add files via upload

--- React/ex1/src/component/Hello.js
+++ React/ex1/src/component/Hello.js
@@ -11,6 +11,16 @@
 import World from "./World";
 import styles from "./Hello.module.css"
 export default function Hello() {
+  function showName() {
+    console.log("Mike");
+  }
+  function showAge(age) {
+    console.log(age)
+  }
+  function showText(e) {
+    console.log(e.target.value);
+  }
+
   return (
     <div>
       <p style={{
@@ -22,8 +32,13 @@ export default function Hello() {
       <div className={styles.box}>
        okay
       </div> 
-      <button>Show name</button>
-      <button>Show age</button>
+      <button onClick={showName}>Show name</button>
+      <button onClick={
+        () => {
+          showAge(20);
+        }
+      }>Show age</button>
+      <input type="text" onChange={showText}></input>
     </div>
   );
 }
--- React/ex1/src/component/Welcome.js
+++ React/ex1/src/component/Welcome.js
@@ -1,3 +1,20 @@
 export default function Welcome() {
-  return <p>Welcome</p>;
+  let age = 26;
+  let beverage = (age >= 21) ? "Beer" : "Juice";
+  let name = "Kate";
+  function changeName() {
+    name = (name==="Kate") ? "Jane" : "Kate";
+    console.log(name);
+    document.getElementById("name").innerText = name;
+  }
+
+  return (
+    <div>
+      <p>Welcome</p>
+      <p id ="name">{name}</p>
+      <p>{age} & {beverage}</p>
+      <button onClick={changeName}>Change</button>
+    </div>  
+  ) 
+
 };

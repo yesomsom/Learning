e18ed8759146b6fb0ad8f26e5d3d6536d0f89a71
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-12-18 00:42:12 +0900

    Add files via upload

--- React/voca/src/component/Day.js
+++ React/voca/src/component/Day.js
@@ -1,12 +1,28 @@
-import dummy from "../db/data.json";
+//import dummy from "../db/data.json";
+//import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"; //url 변수 가져옴
-import Word from "../component/Word";
+import useFetch from "../hooks/useFetch";
+import Word from "./Word";
 
 export default function Day() {
 
   const day = useParams().dayEle;
   //const {day} = useParams();
-  const wordlist = dummy.words.filter(word=>word.day === Number(day)); 
+  //const wordlist = dummy.words.filter(word=>word.day === Number(day)); 
+/*  const [wordlist, setwordlist] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3002/words?day=${day}`)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setwordlist(data);
+    })
+  }, [day]); // day 의존성 추가
+*/
+
+  const wordlist = useFetch(`http://localhost:3002/words?day=${day}`);
 
   return <>
   <h2 className="day_title_color">Day {day}</h2>
--- React/voca/src/component/DayList.js
+++ React/voca/src/component/DayList.js
@@ -1,22 +1,35 @@
 //import dummy from "../db/data.json";
 import { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 export default function DayList() {
   //console.log(dummy);
-  const [days, setDays] = useState([]);
+/*  const [days, setDays] = useState([]);
 
   useEffect(() => {
-    console.log("change");
-  });
+    fetch('http://localhost:3002/days')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setDays(data);
+    })
+  }, []); // 빈배열 -> 처음 호출 시에만 실행
+*/
+
+  const days = useFetch("http://localhost:3002/days");
 
   return (  
-    <ul className="list_day">
-      {days.map(days => (
-        <li className="list_day_li" key={days.id}>
-          <Link to={`/day/${days.day}`} id="fontColorWhite">Day {days.day}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="list_day">
+        {days.map(days => (
+          <li className="list_day_li" key={days.id}>
+            <Link to={`/day/${days.day}`} id="fontColorWhite">Day {days.day}</Link>
+          </li>
+        ))}
+      </ul>    
+    </>
+
   )
 }

fb5b3e79340da77772e2e8917d662c605d41f756
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-12-21 16:23:28 +0900

    Add files via upload

--- React/voca/src/component/DayList.js
+++ React/voca/src/component/DayList.js
@@ -1,5 +1,5 @@
 //import dummy from "../db/data.json";
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 

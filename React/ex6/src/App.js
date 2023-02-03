c8203e95814e62d694610136a3b89e9174cf6e08
yesomsom <109362777+yesomsom@users.noreply.github.com>
2023-02-03 17:31:14 +0900

    Add files via upload

--- React/ex6/src/App.js
+++ React/ex6/src/App.js
@@ -6,21 +6,30 @@ function App() {
   const canvas = useRef(null);
   const [ctx, setCtx] = useState(undefined);
   const [isMouse, setIsMouse] = useState(false);
+  const inputFillRef = useRef(null);
 
+  // 마우스로 그림 그릴 때 실행
   const handleMouseMove = e => {
     if(ctx && isMouse) {
       var rect = canvas.current.getBoundingClientRect();
       ctx.fillStyle = 'black';
       ctx.fillRect(e.clientX-rect.left, e.clientY-rect.top, 2,2);
+      // input에 글씨씀
+      ctx.font = '80px Sans-serif';
+      ctx.fillStyle = 'red';
+      ctx.fillText(inputFillRef.current.value, 400, 400);
+      ctx.textAlign = 'center';
     }
   }
 
+  // 초기화 -> 새로운 캔버스로 덮어씌움
   const handleReset = () => {
     if(ctx) {
       ctx.clearRect(0,0,canvas.current.width, canvas.current.height);
     }
   }
 
+  // 이미지 출력
   const getImage = () => {
     var img = new Image();
     img.onload = function() {
@@ -28,6 +37,8 @@ function App() {
     };
     img.src = CatImage;
   }
+
+  // 랜더링 시 최초 1번
   useEffect(() => {
     setCtx(canvas.current.getContext('2d'));
   },[]);
@@ -48,6 +59,7 @@ function App() {
       />
       <button onClick={handleReset}>초기화</button>
       <button onClick={getImage}>이미지 가져오기</button>
+      <input type="text" ref={inputFillRef}></input>
       {/* <img src={CatImage} /> */}
     </div>
   );

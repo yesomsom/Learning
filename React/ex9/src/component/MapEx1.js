156dcde683c5b32ee04cc1faf418f0b599866e00
yesomsom <109362777+yesomsom@users.noreply.github.com>
2023-02-09 16:42:54 +0900

    Add files via upload

--- React/ex9/src/component/MapEx1.js
+++ React/ex9/src/component/MapEx1.js
@@ -2,14 +2,37 @@ import { useState } from "react";
 
 const MapEx1 = () => {
   const [animal, setAnimal] = useState([
-    {id:1, text:'cat'},
-    {id:2, text:'dog'},
-    {id:3, text:'hamster'},
-    {id:4, text:'snake'}    
-  ])
+    {id:1, text:'고양이'},
+    {id:2, text:'강아지'},
+    {id:3, text:'햄스터'},
+    {id:4, text:'고슴도치'}    
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = e => {
+    setInputText(e.target.value);
+  }
+
+  const onClick = () => {
+    const nextNames = animal.concat({
+      id: nextId,
+      text: inputText
+    });
+    setNextId(nextId + 1);
+    setAnimal(nextNames);
+    setInputText('');
+  };
+
+  const animalList = animal.map(name => <li key={name.id}>{name.text}</li>);
   return (
     <div>
-      
+      <h2>동물</h2>
+      <input value={inputText} onChange={onChange}/>
+      <button onClick={onClick}>추가</button>
+      <ul>
+        {animalList}
+      </ul>
     </div>
   );
 };

83adc17c6add67cabe8f4847369ea0487970fc96
yesomsom <109362777+yesomsom@users.noreply.github.com>
2023-02-10 16:19:09 +0900

    Add files via upload

---
+++ Typescript_React/ts_ex1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
+
+/* typescript */
+// npm install typescript @types/node @types/react @types/react-dom @types/jest @types/react-router-dom

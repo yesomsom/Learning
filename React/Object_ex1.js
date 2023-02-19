5bb1435a28efd483c0caf6fc038e51408f91f8e1
yesomsom <109362777+yesomsom@users.noreply.github.com>
2023-02-19 18:03:40 +0900

    Create Object_ex1.js

---
+++ React/Object_ex1.js
@@ -0,0 +1,21 @@
+const obj = {
+  first: {name: 'aa', price : 1000}, 
+  second: {nickname: 'bb', age : 20},
+  third: {name: 'cc', price: 3000}
+};
+const { first, second, third } = obj;
+
+const ex1 = Object.keys(first); //name,price
+const ex2 = Object.keys(first).filter((key) => first[key]); //name,price
+const ex3 = Object.values(first); //aa,1000
+const ex4 = Object.keys(second).filter((key) => second[key]); //nickname,age
+Object.keys(second).filter((item) => second[item]).map((item) => {
+    console.log(item); // "nickname", "age"
+});
+Object.keys(third).filter((item) => third[item]).map((item) => {
+    console.log(third[item]); // "cc", 3000
+});
+document.writeln("ex1: ", ex1);
+document.writeln("ex2: ", ex2);
+document.writeln("ex3: ", ex3);
+document.writeln("ex4: ", ex4);

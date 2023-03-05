cc0c313fb0510b5670e3b30913e5cf5be3e0be7d
yesomsom <109362777+yesomsom@users.noreply.github.com>
2023-03-05 15:05:09 +0900

    Update Search_Ex2.tsx

--- React/Search_Ex2.tsx
+++ React/Search_Ex2.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useRef, useState } from "react";
 const SearchEx2 = () => {
   const [page, setPage] = useState(0);
   const [allState, setAllState] = useState('');
+  const [allInputs, setAllInputs] = useState({allInput:'',oneInput:''});
+  const { allInput, oneInput } = allInputs;
   const formRef = useRef<HTMLFormElement>(null);
   const all = formRef.current ? (formRef.current?.elements as any).all.value : 'no';
 
@@ -14,7 +16,7 @@ const SearchEx2 = () => {
       onClick={() => {
         const all = formRef.current ? (formRef.current?.elements as any).all.value : 'no';
         setPage(ind + 1);
-        console.log('page', ind+1, 'all', all, allState);
+        console.log('page', page, ind+1, 'all', all, allState, allInputs.allInput, allInputs.oneInput);
       }}
     >
       {ind + 1}
@@ -32,7 +34,6 @@ const SearchEx2 = () => {
         ref={formRef}
         onSubmit={(e: React.FormEvent<HTMLFormElement>)=>{
           e.preventDefault();
-          setPage(0);
           const { all, one, two, three } = (e.currentTarget.elements as any); 
           const oneId = one.checked ? one.id : '';
           const twoId = two.checked ? two.id : '';
@@ -44,6 +45,12 @@ const SearchEx2 = () => {
           console.log('page', page);
           console.log('page 0', 'all', all.value, 'check', oneId,'check', twoId, 'check', threeId);
           setAllState(all.value);
+          setPage(0);
+          setAllInputs({
+            ...allInputs,
+            allInput: all.value,
+            oneInput: one.id
+          })
         }}
       >
         <input name="all"/>

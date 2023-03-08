c2b1f14c387315e04d0465d4a8d8d94ad5e8df53
yesomsom <109362777+yesomsom@users.noreply.github.com>
2023-03-08 18:35:27 +0900

    Update Draw_Ex1.js

--- React/Draw_Ex1.js
+++ React/Draw_Ex1.js
@@ -3,19 +3,32 @@ import person from './person.png';
 const Draw = () => {
   const onClickFun = () => {
     let canvas = document.getElementById('originalDrawCanvas');
+    let tempCanvas = document.createElement('canvas');
+    let target = document.getElementById('uploadImage');
+    const img_temp = new Image();
+    img_temp.src = person;
+    img_temp.onload = (e) => {
+      canvas.width = img_temp.width;
+      canvas.height = img_temp.height;
+      tempCanvas.width = img_temp.width;
+      tempCanvas.height = img_temp.height;
+      let ctx = canvas.getContext('2d');
+      ctx.drawImage(target, 0, 0, img_temp.width, img_temp.height);
+    }
   }
 
   return (
     <>
       <img 
         src={person}
+        id="uploadImage"
       />
       <canvas 
         id='originalDrawCanvas'
       />
-      <canvas 
+      {/* <canvas 
         id='rectDrawCanvas'
-      />
+      /> */}
       <button
         onClick={onClickFun}
       >
